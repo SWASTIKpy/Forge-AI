e5ee83e80d79c2c2cf626508747ce22029eb97fa
swastikPY <swastikvishwa908@gmail.com>
2026-07-31 11:14:12 +0530

    implementing user store login in db

---
+++ lib/checkUser.ts
@@ -0,0 +1,12 @@
+import { currentUser } from "@clerk/nextjs/server";
+
+export const checkUser = async () => {
+    const user = await currentUser();
+    if(!user) return null;
+
+    try{
+
+    } catch (error) {
+    
+    }
+}
